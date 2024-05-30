35ee27f08b09c9bda762f11cece3c9be9640f77f
I9uana0 <ivchi06@gmail.com>
2024-05-30 14:17:29 +0300

    fixed frontend

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://best-pindie-back.nomorepartiesco.ru/api';
+export const BASE_URL = 'https://best-pindie-back.nomorepartiesco.ru/api';
 
 export const endpoints = {
     games: `${BASE_URL}/games`,
